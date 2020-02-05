a1f0011b33ab0c4e2617550fc55a23ad11a68911
s-macias <1552@holbertonschool.com>
2020-02-05 23:18:40 +0000

    edit Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 
-int main
+/**
+ * main - it prints using the puts command
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n);
-	     return 0;
-	     }                
+puts("Programming is like building a multilingual puzzle\n);
+return 0;
+}                
