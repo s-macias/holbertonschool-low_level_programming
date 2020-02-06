1c48892739d33b69498670b10afa9e15dcaa41f4
s-macias <1552@holbertonschool.com>
2020-02-06 16:07:53 +0000

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**                                                                                                                                                 
+* main - it prints a text using printf                                                                                                           
+* Return: Always 0 (Success)                                                                                                                        
+*/
+int main(void)
+{
+printf("Size of a char: '%c' byte(s)",sizeof(char));
+printf("Size of an int: '%d' byte(s)",sizeof(int));
+printf("Size of a long int: '%li' byte(s)",sizeof(long));
+printf("Size of a long long int: '%lli' byte(s)",sizeof(long long int));
+print f("Size of a float: '%f' byte(s)",sizeof(float));
+return(0);
+}#include<stdio.h>
+/**                                                                                                                                                 
+* main - it prints a text using printf                                                                                                           
+* Return: Always 0 (Success)                                                                                                                        
+*/
+int main(void)
+{
+printf("Size of a char: '%c' byte(s)",sizeof(char));
+printf("Size of an int: '%d' byte(s)",sizeof(int));
+printf("Size of a long int: '%li' byte(s)",sizeof(long));
+printf("Size of a long long int: '%lli' byte(s)",sizeof(long long int));
+print f("Size of a float: '%f' byte(s)",sizeof(float));
+return(0);
+}
