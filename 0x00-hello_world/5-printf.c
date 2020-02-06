3607622aff9a26966dce61ffbf9cdf2d8fffbb7b
s-macias <1552@holbertonschool.com>
2020-02-06 01:44:25 +0000

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+\* main - it prints a text using printf
+* Return: Always 0 (Success)
+*\
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
