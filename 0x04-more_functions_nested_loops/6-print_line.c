4214be6f662a42e1060ec7e54defb8c6042e48d3
s-macias <1552@holbertonschool.com>
2020-02-14 21:58:53 +0000

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ */
+
+void print_line(int n)
+{
+	int i = 1;
+
+	if (n != 0)
+	{
+		for (; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
