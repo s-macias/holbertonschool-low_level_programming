0e19970ccdd1a6aaf1cc9ffd60dedb76d00f6114
s-macias <1552@holbertonschool.com>
2020-02-19 05:04:33 +0000

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* puts2 - prints every other character of a string
+* @str : string to be printed
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != 0; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i];
+				 }
+				_putchar('\n');
+				 }
+}
