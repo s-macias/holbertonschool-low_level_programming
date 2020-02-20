55190c8cf1518e1c5b103a944c9c8dc3fc9086b0
s-macias <1552@holbertonschool.com>
2020-02-20 05:46:08 +0000

    removed executable file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_array - prints an array
+* return: 0
+* printf - prints formatted text
+* @a: array to be printed
+* @n: size of the array
+*/
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; *a <= n; ++i)
+	{
+		printf("%d, ", a[i]);
+	}
+}
