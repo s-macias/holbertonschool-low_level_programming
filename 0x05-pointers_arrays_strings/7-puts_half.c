dc55e970c92ac0f653bb340889cf8cf9c40a62da
s-macias <1552@holbertonschool.com>
2020-02-20 05:15:43 +0000

    removed _strlen and added original function

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,21 @@
 #include "holberton.h"
 
 /**
-* puts_half - prints the second half a string
-* _strlen - returns the length of a string
-* @str : string to be printed
+* puts_half - prints the second half of a string
+* return: 0
+* _putchar - prints a character
+* @str: string to print
 */
 
 void puts_half(char *str)
 {
+	int length_of_the_string = 0;
 	int n;
-	int length_of_the_string;
 
-	length_of_the_string = _strlen(str);
+	for (; *str != '\0'; ++length_of_the_string)
+	{
+		str++;
+	}
 	n = length_of_the_string / 2;
 
 	for (; n <= length_of_the_string - 1; n++)
