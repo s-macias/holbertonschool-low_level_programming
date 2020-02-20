c873c9c86e1adebc9b8872275a5248d48f388149
s-macias <1552@holbertonschool.com>
2020-02-20 01:31:14 +0000

    modified initialization value of length in second for loop

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,19 +6,16 @@
 * return: 0 for success
 * @s: string to be printed
 * @length: length of the string
-* @i: counter
 */
 
 void print_rev(char *s)
 {
 	int length = 0;
-	int i;
 
-	for (; s[length] != '\0'; length++)
-	i = length - 1;
-	for (; i >= 0; i--)
+	for (; s[length] != '\0'; length++);
+	for (; length >= 0; length--)
 	{
-		_putchar(s[i]);
+		_putchar(s[length]);
 	}
 	_putchar('\n');
 }
