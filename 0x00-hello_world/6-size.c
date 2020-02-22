ebe1e5f83013ef3be1c3317b483defed51491eb9
s-macias <1552@holbertonschool.com>
2020-02-22 22:07:48 +0000

    Added declaration of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,19 @@
 * printf - prints formatted text
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-	printf("Size of a char: '%d' byte(s)\n", sizeof(char));
-	printf("Size of an int: '%d' byte(s)\n", sizeof(int));
-	printf("Size of a long int: '%d' byte(s)\n", sizeof(long));
-	printf("Size of a long long int: '%d' byte(s)\n", sizeof(long long int));
-	printf("Size of a float: '%d' byte(s)\n", sizeof(float));
+	char cha;
+	int inte;
+	long lon;
+	long long ll;
+	float floa;
+
+	printf("Size of a char: '%d' byte(s)\n", sizeof(cha));
+	printf("Size of an int: '%d' byte(s)\n", sizeof(inte));
+	printf("Size of a long int: '%d' byte(s)\n", sizeof(lon));
+	printf("Size of a long long int: '%d' byte(s)\n", sizeof(ll));
+	printf("Size of a float: '%d' byte(s)\n", sizeof(floa));
 	return (0);
 }
