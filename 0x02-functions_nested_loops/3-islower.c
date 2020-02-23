71002d7b198636970f497f8e23063d66f49eb1d7
s-macias <1552@holbertonschool.com>
2020-02-23 04:04:33 +0000

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for lowercase character.
+ * _putchar - prints characters
+ * Return: 1 for lowercase
+ * Return: 0 if not lowercase
+ * @c: character to check
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+	_putchar('\n');
+}
