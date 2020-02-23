88f565e96c902449a197922d9d9d202e6fee96c7
s-macias <1552@holbertonschool.com>
2020-02-23 04:43:58 +0000

    edited comments

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,13 @@
 #include "holberton.h"
 
 /**
- * _islower - checks for lowercase character.
- * _putchar - prints characters
- * Return: 1 for lowercase
- * Return: 0 if not lowercase
- * @c: character to check
- */
+* _isalpha - checks for alphabetic character.
+* Return: 1 for alphabetic character, 0 if otherwise
+* @c: character to check
+*/
 
 int _isalpha(int c)
 {
-
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
