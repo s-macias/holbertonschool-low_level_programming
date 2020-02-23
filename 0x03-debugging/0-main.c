a88f463521756a801145e82bf4324e28c5be549a
s-macias <1552@holbertonschool.com>
2020-02-23 07:58:55 +0000

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - test for a postitive_or_negative function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
