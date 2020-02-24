62b047f04fa2f9403b3145e1259d0e26280d3c34
s-macias <1552@holbertonschool.com>
2020-02-24 17:03:32 +0000

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * *_memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: memory area to copy to
+ * @src: memory area to copy from
+ * @n: number of bytes in the memory area pointed to by s
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i <= n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i <= n; i++)
+		dest[i] = '-';
+	return (dest);
+}
