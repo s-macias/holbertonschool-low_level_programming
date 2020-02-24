686cecacfce4456a7f601286f0f9966aff9e2452
s-macias <1552@holbertonschool.com>
2020-02-24 20:07:56 +0000

    changed return value

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string where the character will be searched
+ * @c: character to be searched
+ * Return: pointer to first occurrence of c or NULL if there is none
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
