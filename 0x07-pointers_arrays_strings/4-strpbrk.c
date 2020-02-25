1e1bd5d0db3cb1c34c0a844ee520b7087feaf2df
s-macias <1552@holbertonschool.com>
2020-02-25 01:57:52 +0000

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk  - searches a string for any of a set of bytes
+ * @s: string where the character will be searched
+ * @accept: string to be searched
+ * Return: n, pointer to the first occurence of s or NULL if there is none
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *n;
+	n = accept;
+
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*accept == *s)
+			{
+				return (s);
+			}
+			accept++;
+		}
+		accept = n;
+		s++;
+	}
+	return ('\0');
+}
