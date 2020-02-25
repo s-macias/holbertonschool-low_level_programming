9d95ff819b6cd451588e5027df5098aa799e4221
s-macias <1552@holbertonschool.com>
2020-02-25 22:30:32 +0000

    Task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 
 /**
  * print_chessboard - prints the chessboard
- * _putchar
+ * _putchar - prints a character
  * @a: character
  */
 
@@ -10,22 +10,12 @@ void print_chessboard(char (*a)[8])
 {
 	int r, c;
 
-	 a[8][8] = {
-		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-	};
 	for (r = 0; r < 8; r++)
 	{
 		for (c = 0; c < 8; c++)
 		{
 			_putchar(a[r][c]);
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
