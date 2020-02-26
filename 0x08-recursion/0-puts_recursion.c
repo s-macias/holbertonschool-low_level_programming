e2d718987fcd18ae44e538bf1fa048b47dea5b34
s-macias <1552@holbertonschool.com>
2020-02-26 16:14:37 +0000

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ * _putchar - prints a character
+ * @s: string to be printed
+ * Return: 0 for success
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}
