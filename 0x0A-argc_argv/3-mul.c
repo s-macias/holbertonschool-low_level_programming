f54641e2c00d7c3b9acab37aed829a0ce3871914
s-macias <1552@holbertonschool.com>
2020-03-02 04:50:09 +0000

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "holberton"
+#include <stdio.h>
+
+/**
+ * main -  multiplies two numbers.
+ * @argc: counts the number of arguments
+ * @argv: argument vector: program name and arguments
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+
+	int a, b, sum;
+
+	if (argc > 1)
+	{
+		a = atoi(arg[1]);
+		b = atoi(arg[2]);
+		sum = a * b;
+		printf("%d\n", count, sum);
+	return (0);
+	}
+}
