c86a699a5c522518fd2e134d05278292b1631df4
s-macias <1552@holbertonschool.com>
2020-03-02 04:33:09 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: counts the number of arguments
+ * @argv: argument vector: program name and arguments
+ * Return: 0
+ */
+
+int main (int argc, char *arg[])
+{
+	if (argc > 0)
+		printf(" %s\n", argv[0]);
+
+	return 0;
+}
