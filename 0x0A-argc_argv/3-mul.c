e8166259d13ac62992d48bf409a52997e60674f1
s-macias <1552@holbertonschool.com>
2020-03-03 03:57:58 +0000

    included stdlib.h

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,27 @@
-#include "holberton"
+#include "holberton.h"
+#include <stdlib.h>
 #include <stdio.h>
-
 /**
  * main -  multiplies two numbers.
+ * atoi - turns a string into an integer
  * @argc: counts the number of arguments
  * @argv: argument vector: program name and arguments
  * Return: 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 
 	int a, b, sum;
 
-	if (argc > 1)
+	if (argc >= 1)
 	{
-		a = atoi(arg[1]);
-		b = atoi(arg[2]);
-		sum = a * b;
-		printf("%d\n", count, sum);
-	return (0);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		sum = (a * b);
+		printf("%d\n", sum);
 	}
+	else
+		printf("Error\n");
+	return (0);
 }
