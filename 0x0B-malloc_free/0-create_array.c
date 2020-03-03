51ede1f37ced1602d4d697315056cdd061db5b87
s-macias <1552@holbertonschool.com>
2020-03-03 20:07:25 +0000

    added return for NULL value

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,15 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s;
+	unsigned int i = 0;
 
+	if (size == 0)
+		return (NULL);
 	s = malloc(sizeof(char) * size);
-	s[0] = c;
+	if (s == 0)
+		return (NULL);
+	while (i < size)
+		i++;
+	s[i] = c;
 	return (s);
-
-
 }
