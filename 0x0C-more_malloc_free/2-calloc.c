d9c8b0777f7fd4059061874a2e76bf3cd286628e
s-macias <1552@holbertonschool.com>
2020-03-05 21:54:42 +0000

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: number of elements of the array
+ * @size: bytes of the elements
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int i = 0;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return (NULL);
+	while (i < nmemb)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return (p);
+}
