3b97bdc57ece7bd3555f8bf28f726a3bd53c16c7
s-macias <1552@holbertonschool.com>
2020-03-10 04:48:34 +0000

    addedm prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 #include "dog.h"
-#include <stdlib.h>
 
 /**
- * struc  dog - intilializes a variable type struct dog
+ * init_dog - intilializes a variable type struct dog
  * @d: struct to be initialized
  * @name: name of the dog
  * @age: age of the dog
  * @owner dog's owner
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	dog_->name = name;
-	dog_->age = age;
-	dog_ ->owner = owner;
+	char *name;
+	float age;
+	char *owner;
 }
+
+	void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
