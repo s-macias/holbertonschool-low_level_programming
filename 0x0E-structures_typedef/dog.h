bb7d83194875e1e7e82e356c9bb6a73d37a02f06
s-macias <1552@holbertonschool.com>
2020-03-11 02:47:34 +0000

    used typedef and included function prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#include "dog.h"
 
 /**
  * init_dog - intilializes a variable type struct dog
@@ -17,4 +16,9 @@ struct dog
 	char *owner;
 }
 
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t, *d);
 #endif
