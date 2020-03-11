d3e15f5dba29a0cfee62ed551166aeee5d62396c
s-macias <1552@holbertonschool.com>
2020-03-11 03:34:59 +0000

    removed if statement

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
  * init_dog - intilializes a variable type struct dog
  * @d: pointer to struct to be initialized
@@ -15,11 +16,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d == malloc(sizeof(struct dog));
 	if (d == NULL)
 		return;
-
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
