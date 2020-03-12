a3d8233db336c9308fb0bd231e028ed4caa44031
s-macias <1552@holbertonschool.com>
2020-03-12 22:23:03 +0000

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,12 +6,13 @@
  * main - performs simple operations
  * @argc: number of parameters
  * @argv: pointer to array with arguments
- * Return: 0 for success
+ * Return: result of the operation
  */
 
 int main (int argc, char *argv[])
 {
-	int (* calc)(int, int);
+	int num1, num2, total;
+	int (* opera)(int, int);
 
 	if (argc != 4)
 	{
@@ -19,12 +20,20 @@ int main (int argc, char *argv[])
 		exit(98);
 	}
 	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
 		exit(99);
-
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	calc = get_op_func(operator)(argv[2]);
-	printf("%d\n", calc);
+	opera = get_op_func(argv[2]);
+	if (opera == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	total = opera(num1, num2);
+	printf("%d\n", total);
 	return (0);
 }
