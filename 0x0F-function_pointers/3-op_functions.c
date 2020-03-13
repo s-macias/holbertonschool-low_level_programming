6965405c8b2574cee8940dcfc4509f47ceb575b5
s-macias <1552@holbertonschool.com>
2020-03-13 02:34:38 +0000

    Task 3 added error message

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -40,13 +40,14 @@ int op_mul(int a, int b)
  * op_div - returns the result of the division of a by b
  * @a: dividend
  * @b: divisor
- * Return: resul of the division
+ * Return: result of the division
  */
 
 int op_div(int a, int b)
 {
 	if (b == 0)
 	{
+		printf("Error\n");
 		exit(100);
 	}
 	return (a / b);
@@ -63,6 +64,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
+		printf("Error\n");
                 exit(100);
 	}
 	return (a % b);
