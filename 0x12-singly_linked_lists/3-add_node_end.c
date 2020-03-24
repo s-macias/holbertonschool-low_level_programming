fa874a51cf02f1ca63e2c15121452f3577d64a41
s-macias <1552@holbertonschool.com>
2020-03-24 17:58:54 +0000

    added while loop

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,21 +9,32 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int node_number = 0;
+	unsigned int string_length = 0;
 	list_t *new_node;
+	list_t *temp;
 
+	if (str == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
-
-	while (str[node_number])
+	while (str[string_length])
 	{
-		node_number++;
+		string_length++;
 	}
-	new_node->len = node_number;
+	new_node->len = string_length;
 	new_node->str = strdup(str);
+	if (*head == NULL)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	new_node->next = NULL;
-	*head = new_node;
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new_node;
 	return (new_node);
 }
