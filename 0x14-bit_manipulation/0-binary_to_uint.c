6fa42c05fdc36903d5bba9bfb8449ab1bd8991da
s-macias <1552@holbertonschool.com>
2020-03-29 04:51:05 +0000

    corrected Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
- * pow - calculates the power raised to a base value
  * @b: pointer to a string of ones and zeros (the binary number to convert)
  *
  * Return: the converted number or 0 if there are chars different from 1 or 0
@@ -11,21 +11,23 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int length = 0, exp = 0;
+	int length = 0, m = 1;
 	unsigned int n = 0;
 
 	if (b == NULL)
 		return (0);
-	while (*b && *b != 0 && *b != 1)
+	while (b[length] != '\0')
 	{
-		if (*b != 1 && *b != 0)
-			return (0);
 		length++;
 	}
-	for (; length >= 0; length--)
+	while (length)
 	{
-		n += b[length] << exp;
-		exp++;
+		if (!(b[length - 1] == '1' || b[length - 1] == '0'))
+			return (0);
+		if (b[length - 1] == '1')
+			n += m;
+		m *= 2;
+		length--;
 	}
 	return (n);
 }
