43a2c3ab039f051209e4bd7a0db7f9173ad63b7a
s-macias <1552@holbertonschool.com>
2020-03-30 03:41:05 +0000

    added condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,6 +39,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = temp->next;
 	}
+	if (idx > i)
+		return (NULL);
 	new_node->n = n;
 	new_node->next = temp->next;
 	temp->next = new_node;
