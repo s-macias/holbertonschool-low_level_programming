a4b838197f116e6f8251c627964673d0c300831c
s-macias <1552@holbertonschool.com>
2020-03-31 23:07:23 +0000

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to the file that will be read and printed
+ * @letters: number of letters to read and print or 0 if the file cannot be
+opened or read or number of bytes is not what is expected
+ *
+ * Return: number of bytes returned
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *string;
+	int error, fd, read_letters, written_letters;
+
+	if (filename == NULL)
+		return (0);
+
+	string = malloc(sizeof(char) * letters);
+
+	if (string == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0 )
+		return (0);
+
+	read_letters = read(fd, string, letters);
+	if (read_letters < 0)
+		return (0);
+	written_letters = write(STDOUT_FILENO, string, read_letters);
+	if (written_letters < 0)
+		return (0);
+
+	error = close(fd);
+	if (error < 0)
+		return (0);
+
+	free(string);
+	return (written_letters);
+}
