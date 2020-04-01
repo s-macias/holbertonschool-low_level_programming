ce2859fa5387f12fb3f8927d5bc77d2692261396
s-macias <1552@holbertonschool.com>
2020-04-01 01:01:43 +0000

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: pointer to the file that will be read and printed
+ * @text_contet pointer to the content of the file
+ *
+ * Return: 1 for success, -1 for failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int text_length = 0, fd, written_letters;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content)
+		text_length++;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+
+	written_letters = write(fd, text_content, text_length);
+	if (written_letters < 0)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
