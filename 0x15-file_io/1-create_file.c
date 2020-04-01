4f8de354339cf942fc434ceef9596681eb7fb963
s-macias <1552@holbertonschool.com>
2020-04-01 01:40:45 +0000

    fixed while condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates a file
  * @filename: pointer to the file that will be read and printed
- * @text_contet pointer to the content of the file
+ * @text_content pointer to the content of the file
  *
  * Return: 1 for success, -1 for failure
  */
@@ -15,15 +15,18 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content)
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[text_length])
 		text_length++;
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd < 0)
+	if (fd == -1)
 		return (-1);
 
 	written_letters = write(fd, text_content, text_length);
-	if (written_letters < 0)
+	if (written_letters == -1)
 		return (-1);
 
 	close(fd);
