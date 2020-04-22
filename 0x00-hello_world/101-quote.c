d6a4f39973a90e35f4db1b3c64083564b54554de
s-macias <1552@holbertonschool.com>
2020-04-22 22:53:21 +0000

    adding task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,26 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - entry point
- * putchar - prints characters
- * Return: 1
- * @quote: string to be printed
+ * main - print a quote without using printf
+ * 
+ * Return: 1 for success
  */
 
-int main(char * quote)
+int main(void)
 {
-
-	* quote = "and that piece of art is useful\" - Dora Korpar, 2015-10/-19";
-	int counter = 0;
-	int i = 0;
-
-	while (quote[counter] != '\0')
-		counter++;
-
-	for (; i <= counter; i++)
-	{
-		putchar(quote[i]);
-	}
-putchar('\n');
-return(1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
