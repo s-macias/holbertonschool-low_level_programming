8f3c8f97ce4740c40ea32543385da2158541bcb2
s-macias <1552@holbertonschool.com>
2020-04-23 01:29:58 +0000

    v.1 task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - prints the n-times table, starting with 0
+ * @n: number whose times table we need to print
+ *
+ * Return: nothing
+ */
+
+void print_times_table(int n)
+{
+	int m1, m2, m;
+
+	if (n < 0  && n > 15)
+		return;
+}
