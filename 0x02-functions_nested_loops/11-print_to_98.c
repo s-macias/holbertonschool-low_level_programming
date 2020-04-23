cf49dbe60fa298f71e13447bd57a5b4dbcf775ad
s-macias <1552@holbertonschool.com>
2020-04-23 00:49:43 +0000

    corrected typo

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
 * print_to_98 - it prints n-98 using standard library
+* printf - prints formatted text
 * @n: first number in the sequence
 * Return: nothing
 */
 
 void print_to_98(int n)
 {
-	int i;
-
 	if (n <= 98)
 	{
 		while (n <= 98)
 		{
 			printf("%d, ", n);
 			if (n != 98)
-				prinf(", ");
+				printf(", ");
 			n++;
 		}
 	}
