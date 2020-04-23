06d3a4f4bc21614bdbf7241fb5927f8d7b7f9726
s-macias <1552@holbertonschool.com>
2020-04-23 02:52:09 +0000

    fixing task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,37 +1,30 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - prints numbers 1-100 substituting multiples of 3, 5 and both.
+ *
+ * Return: 0 for success
  */
 
 int main(void)
-
+{
 	int i = 1;
 
-for (; i <= 100; i++)
-
-{
-
-	if (i % 3 == 0 && i % 5 == 0)
-
-		printf("FizzBuzz");
-
-	else if (i % 3 == 0)
-
-		printf("Fizz");
-
-	else if (i % 5 == 0)
-
-		printf("Buzz");
-
-	else
-
-		printf(«%d», i);
-
-	if (i < 100)
-
-		putchar(32);
+	for (; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			putchar(32);
+	}
+	printf("\n");
+	return (0);
 }
-
-putchar(‘\n');
