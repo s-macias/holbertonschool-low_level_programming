5b3a0fc49204788f2b28250d5bcd3818c40f1696
s-macias <1552@holbertonschool.com>
2020-04-24 02:01:31 +0000

    fixed leak

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,11 +11,11 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
+	temp = head;
 	while (head)
 	{
 		temp = head;
 		head = head->next;
-		free(head->next);
 		free(temp->str);
 		free(temp);
 	}
