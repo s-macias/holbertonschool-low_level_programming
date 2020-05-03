51ef685a442b12d194c846f186562180ced59600
s-macias <1552@holbertonschool.com>
2020-05-03 04:20:47 +0000

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,11 +13,11 @@ size_t print_dlistint(const dlistint_t *h)
 
 	if (h == NULL)
 		return (0);
-	while(h)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		node_number++;
 	}
-	return(node_number);
+	return (node_number);
 }
