cf983ad363e1b48e8cdccc19b1babaf52400e6a4
s-macias <1552@holbertonschool.com>
2020-05-19 04:19:42 +0000

    fixed typo

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,14 +6,15 @@
  * @d: pointer to struct to be initialized
  * @name: name of the dog
  * @age: age of the dog
- * @owner dog's owner
+ * @owner: dog's owner
+ *
  * Return: 0
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		d == malloc(sizeof(struct dog));
+		d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		return;
 	d->name = name;
