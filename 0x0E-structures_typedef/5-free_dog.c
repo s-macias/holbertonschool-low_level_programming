30f64c284e2c79d01be033de7a8c9772cd486afa
s-macias <1552@holbertonschool.com>
2020-05-19 05:04:05 +0000

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog struct
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
+}
